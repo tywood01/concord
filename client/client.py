50b943eeb6af8dccc98ce82bc45248ae4790f380
Tytus Woodburn <tytus.woodburn@student.cune.edu>
2025-02-24 08:10:52 -0600

    Init commit

---
+++ client/client.py
@@ -0,0 +1,57 @@
+"""
+web_server.py
+Author: Tytus Woodburn
+CS 341 Computer Networks
+Lab 4
+
+"""
+
+# Available Ports: Tytus: 10261 through 10280
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+
+# about what kind of connection you are making.
+# What are AF_INET and SOCK_STREAM?
+# AF_INET is an address family that can designate the type of
+# adresses your socket can communnicate with.
+# AF_INET: address format is host and port number
+# SOCK_STREAM is a connection-oriented TCP protocol stream.
+SERVER_SOCKET = socket(AF_INET, SOCK_STREAM)
+SERVER_PORT = 10261
+
+# Bind the socket to server address and server port
+SERVER_SOCKET.bind(("localhost", SERVER_PORT))
+
+# Listen to at most 1 connection at a time
+SERVER_SOCKET.listen(1)
+
+
+def send_message(socket, message):
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(("localhost", 10261))
+    clientsocket.send("hello")
+
+
+def recieve_messages():
+    while True:
+        print("Ready to serve...")
+        # Set up a new connection from the client
+        connection_socket, addr = SERVER_SOCKET.accept()
+        print("Got connection from", addr)
+
+        # Receive and decode the request message from the client
+        data = connection_socket.recv(1024)
+        str_data = data.decode()
+        print("from client: ", str_data)
+        connection_socket.close()
+
+    # Close the server socket
+    SERVER_SOCKET.close()
+
+
+def main():
+    recieve_messages()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/server.py
@@ -0,0 +1,45 @@
+"""
+web_server.py
+Author: Tytus Woodburn
+CS 341 Computer Networks
+Lab 4
+
+"""
+
+# Available Ports: Tytus: 10261 through 10280
+from socket import socket, AF_INET, SOCK_STREAM
+import socket
+
+import threading
+
+
+users = {1: "Tytus", 2: "John", 3: "Jane"}
+
+
+def send_message(message):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect the socket to the server's address and port
+        client_socket.connect(("localhost", 10261))
+
+        # Send data to the server
+        message = "Hello, server1!"
+        print(f"Sending: {message}")
+        client_socket.send(message.encode())
+
+    finally:
+        # Clean up the connection
+        client_socket.close()
+
+
+def recieve_messages():
+    pass
+
+
+def main():
+    send_message("hello")
+
+
+if __name__ == "__main__":
+    main()
