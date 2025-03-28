b988537c74fc9e7906836d3bc8b3ae5e82872b08
Mason <mason.fortney@student.cune.edu>
2025-03-28 08:14:07 -0500

    setting.py added

---
+++ setting.py
@@ -0,0 +1,12 @@
+"""
+Template for settings.py
+Create a new file called settings.py in the root directory
+Copy the following code into settings.py
+Replace your_host with the IP address or hostname of the server
+Replace your_database_name with the name of the SQLite database
+Save the file
+"""
+
+HOST = "10.0.16.1"
+PORT = 10261
+DATABASE = "concord.db"
